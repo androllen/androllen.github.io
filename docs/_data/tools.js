d9156d67774a24a5368de6f9f76528a94cda38e9
androllen <androllen@hotmail.com>
2026-01-07 00:21:53 +0800

    post

--- docs/_data/tools.js
+++ docs/_data/tools.js
@@ -5,6 +5,8 @@ export const browserUtility = [
     name: 'Firefox',
     title: '浏览器',
     desc: '开源免费的网页浏览器，支持跨平台使用',
+    orgLink: 'http://ftp.mozilla.org/pub/firefox/releases/75.0/win64/en-US/',
+    org: 'mozilla',
   },
 ];
 
@@ -15,18 +17,24 @@ export const databaseUtility = [
     name: 'SQLitebrowser',
     title: 'SQLite数据库工具',
     desc: '可视化的SQLite数据库管理工具',
+    orgLink: 'https://sqlitebrowser.org/dl/',
+    org: 'sqlitebrowser',
   },
   {
     avatar: 'https://i.imgur.com/8bZ8y7F.png', // SQLite 图标
     name: 'Sqlite SDK',
     title: 'Sqlite 开发工具包',
     desc: 'SQLite数据库的官方开发工具包',
+    orgLink: 'https://www.sqlite.org/download.html',
+    org: 'sqlite',
   },
   {
     avatar: 'https://i.imgur.com/5tB7s9X.png', // HeidiSQL 图标
     name: 'HeidiSQL',
     title: '多数据库管理工具',
     desc: '轻量级的 MariaDB、MySQL、SQL Server、PostgreSQL 和 SQLite 管理工具',
+    orgLink: 'https://github.com/HeidiSQL/HeidiSQL',
+    org: 'HeidiSQL',
   },
 ];
 
@@ -37,12 +45,16 @@ export const remoteUtility = [
     name: 'WinSCP',
     title: 'SFTP客户端',
     desc: 'SSH 的开源图形化 SFTP 客户端，支持文件传输和管理',
+    orgLink: 'https://winscp.net/eng/download.php',
+    org: 'winscp',
   },
   {
     avatar: 'https://i.imgur.com/3s7d89K.png', // Xshell/Xftp 图标
     name: 'Xshell and Xftp',
     title: 'SSH/SFTP/FTP客户端',
     desc: '个人使用免费的SSH终端和文件传输工具',
+    orgLink: 'https://www.netsarang.com/zh/free-for-home-school/',
+    org: 'netsarang',
   },
 ];
 
@@ -53,6 +65,8 @@ export const searchUtility = [
     name: 'Everything',
     title: '文件搜索工具',
     desc: '快速高效的本地文件搜索工具，支持秒级检索',
+    orgLink: 'http://www.voidtools.com/downloads/',
+    org: 'voidtools',
   },
 ];
 
@@ -63,6 +77,8 @@ export const usbFormattingUtility = [
     name: 'Rufus',
     title: 'USB格式化工具',
     desc: '帮助格式化和创建可引导USB闪存盘的轻量级工具',
+    orgLink: 'https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe',
+    org: 'pbatard',
   },
 ];
 
@@ -73,6 +89,8 @@ export const textEditorUtility = [
     name: 'SublimeText3',
     title: '文本编辑器',
     desc: '轻量级、高性能的代码和文本编辑器，支持丰富的插件扩展',
+    orgLink: 'http://www.sublimetext.com/3',
+    org: 'sublimetext',
   },
 ];
 
@@ -83,6 +101,8 @@ export const codeManageUtility = [
     name: 'Git',
     title: '代码管理工具',
     desc: '分布式版本控制系统，用于高效管理代码版本',
+    orgLink: 'https://git-scm.com',
+    org: 'git-scm',
   },
 ];
 
@@ -93,18 +113,24 @@ export const clearUtility = [
     name: 'SpaceSniffer',
     title: '硬盘占用分析工具',
     desc: '直观可视化查看硬盘空间占用情况的工具',
+    orgLink: 'https://pc.qq.com/search.html#!keyword=SpaceSniffer',
+    org: 'qq',
   },
   {
     avatar: 'https://i.imgur.com/9k8j765.png', // DISM++ 图标
     name: 'Dism++',
     title: '系统清理工具',
     desc: 'Windows系统垃圾清理、优化和维护工具',
+    orgLink: 'http://www.chuyu.me/zh-Hans/',
+    org: 'chuyu',
   },
   {
     avatar: 'https://i.imgur.com/0l9k876.png', // Windows Installer Clean Up 图标
     name: 'Windows Installer Clean Up',
     title: '软件卸载工具',
     desc: '专门用于卸载Microsoft系列软件的工具',
+    orgLink: '', // 未提供具体地址
+    org: 'microsoft',
   },
 ];
 
@@ -115,18 +141,24 @@ export const captureDataUtility = [
     name: 'Wireshark',
     title: '网络抓包工具',
     desc: '功能强大的网络数据包分析和抓取工具',
+    orgLink: 'https://www.wireshark.org/download.html',
+    org: 'wireshark',
   },
   {
     avatar: 'https://i.imgur.com/2n0m987.png', // mitmproxy 图标
     name: 'mitmproxy',
     title: '中间人代理抓包工具',
     desc: '支持HTTP/HTTPS的交互式中间人代理工具',
+    orgLink: 'https://www.mitmproxy.org/',
+    org: 'mitmproxy',
   },
   {
     avatar: 'https://i.imgur.com/3p1n098.png', // BurpSuite 图标
     name: 'BurpSuite',
     title: 'Web安全测试工具',
     desc: '用于Web应用程序安全测试的抓包和渗透测试工具',
+    orgLink: 'https://portswigger.net/burp',
+    org: 'portswigger',
   },
 ];
 
@@ -137,30 +169,40 @@ export const downloadUtility = [
     name: 'Curl',
     title: '命令行下载工具',
     desc: '支持多种协议的命令行文件传输工具',
+    orgLink: 'https://curl.haxx.se/download.html',
+    org: 'haxx',
   },
   {
     avatar: 'https://i.imgur.com/5r3q210.png', // IDM 图标
     name: 'IDM',
     title: '桌面下载工具',
     desc: 'Internet Download Manager，高速多线程下载工具',
+    orgLink: 'https://www.softpedia.com/get/Internet/Download-Managers/Internet-Download-Manager.shtml',
+    org: 'softpedia',
   },
   {
     avatar: 'https://i.imgur.com/6s4r321.png', // Homebrew 图标
     name: 'Homebrew',
     title: '命令行包管理工具',
     desc: 'macOS/Linux系统的命令行包管理和下载工具',
+    orgLink: 'http://brew.sh/index_zh-cn.html',
+    org: 'brew',
   },
   {
     avatar: 'https://i.imgur.com/7t5s432.png', // qBittorrent 图标
     name: 'Qbittorrent',
     title: '种子下载工具',
     desc: '开源免费的种子和磁力链接下载工具',
+    orgLink: 'https://www.qbittorrent.org/download.php',
+    org: 'qbittorrent',
   },
   {
     avatar: 'https://i.imgur.com/8u6t543.png', // 占位图标
     name: 'Torrent to Magnet',
     title: '种子转磁力链接工具',
     desc: '将Torrent种子文件转换为磁力链接的工具（暂未找到下载地址）',
+    orgLink: 'http://www.torrent.org.cn/',
+    org: 'torrent',
   },
 ];
 
@@ -171,12 +213,16 @@ export const imagesUtility = [
     name: 'MSDN I tell you',
     title: 'Windows ISO镜像站',
     desc: '提供Windows系统ISO镜像文件的下载站点',
+    orgLink: 'https://msdn.itellyou.cn/',
+    org: 'itellyou',
   },
   {
     avatar: 'https://i.imgur.com/0w8v765.png', // DriverBooster 图标
     name: 'DriverBoosterPortable',
     title: '驱动更新软件',
     desc: '免费的便携版驱动检测和更新工具',
+    orgLink: 'https://www.aliyundrive.com/',
+    org: 'aliyundrive',
   },
 ];
 
@@ -187,12 +233,16 @@ export const videoMusicUtility = [
     name: 'FFmpeg',
     title: '音视频编解码工具',
     desc: '记录、转换数字音频/视频并转化为流的开源程序',
+    orgLink: 'https://ffmpeg.zeranoe.com/builds',
+    org: 'zeranoe',
   },
   {
     avatar: 'https://i.imgur.com/2y0x987.png', // SoX 图标
     name: 'SoX',
     title: '音频格式转换工具',
     desc: '跨平台的命令行音频格式转换和处理工具',
+    orgLink: 'http://sox.sourceforge.net',
+    org: 'sourceforge',
   },
 ];
 
@@ -203,12 +253,16 @@ export const graphicsLibrary = [
     name: 'Cairo',
     title: '2D图形库',
     desc: '支持多种输出设备的2D图形渲染库',
+    orgLink: 'https://www.lfd.uci.edu/~gohlke/pythonlibs/#pycairo',
+    org: 'uci',
   },
   {
     avatar: 'https://i.imgur.com/4a2z109.png', // Manim 图标
     name: 'Manim',
     title: '数学公式图形工具',
     desc: '基于Python的数学动画和公式图形制作工具',
+    orgLink: 'https://www.bilibili.com/video/BV1W4411Z7Zt',
+    org: 'bilibili',
   },
 ];
 
@@ -219,6 +273,8 @@ export const protectDotNetUtility = [
     name: 'ILProtector',
     title: '.NET应用加壳工具',
     desc: '用于保护.NET应用程序的加壳工具',
+    orgLink: 'http://www.vgrsoft.net/Download/ILProtector',
+    org: 'vgrsoft',
   },
 ];
 
@@ -229,66 +285,88 @@ export const devOfficial = [
     name: 'Yasm',
     title: '汇编器工具',
     desc: '模块化的汇编器和反汇编器工具',
+    orgLink: 'http://yasm.tortall.net/',
+    org: 'tortall',
   },
   {
     avatar: 'https://i.imgur.com/7d5c432.png', // MSYS2 图标
     name: 'MSYS2',
     title: 'Windows开发环境',
     desc: 'Windows下的类Unix开发环境',
+    orgLink: 'http://msys2.github.io/',
+    org: 'github',
   },
   {
     avatar: 'https://i.imgur.com/8e6d543.png', // MinGW 图标
     name: 'MinGW',
     title: 'Windows编译工具集',
     desc: 'Minimalist GNU for Windows，Windows下的GNU编译工具集',
+    orgLink: 'http://www.mingw.org/',
+    org: 'mingw',
   },
   {
     avatar: 'https://i.imgur.com/9f7e654.png', // Python 图标
     name: 'Python',
     title: '编程语言',
     desc: '跨平台的解释型高级编程语言',
+    orgLink: 'https://www.python.org/',
+    org: 'python',
   },
   {
     avatar: 'https://i.imgur.com/0g8f765.png', // NuGet 图标
     name: 'NuGet',
     title: '包管理工具',
     desc: '.NET平台的开源包管理系统',
+    orgLink: 'https://www.nuget.org/',
+    org: 'nuget',
   },
   {
     avatar: 'https://i.imgur.com/1h9g876.png', // UMLet 图标
     name: 'UMLet',
     title: 'UML图表工具',
     desc: '快速生成UML图表的轻量级工具',
+    orgLink: 'http://www.umlet.com',
+    org: 'umlet',
   },
   {
     avatar: 'https://i.imgur.com/2i0h987.png', // Docker 图标
     name: 'Docker',
     title: '容器化工具',
     desc: '应用容器化部署和管理工具',
+    orgLink: 'https://docs.docker.com/engine/installation/',
+    org: 'docker',
   },
   {
     avatar: 'https://i.imgur.com/3j1i098.png', // 微信公众平台 图标
     name: '微信公众平台',
     title: '微信开发平台',
     desc: '微信公众号/小程序等开发管理平台',
+    orgLink: 'https://mp.weixin.qq.com/',
+    org: 'weixin',
   },
   {
     avatar: 'https://i.imgur.com/4k2j109.png', // MiKTeX 图标
     name: 'MiKTeX',
     title: 'LaTeX排版工具',
     desc: '用于处理文字信息的LaTeX排版系统',
+    orgLink: 'https://miktex.org/download',
+    org: 'miktex',
   },
   {
     avatar: 'https://i.imgur.com/5l3k210.png', // Tomcat 图标
     name: 'Tomcat',
     title: 'Web服务器',
     desc: 'Apache Tomcat，开源的Java Servlet容器',
+    orgLink: 'https://archive.apache.org/dist/tomcat/',
+    org: 'apache',
   },
   {
     avatar: 'https://i.imgur.com/6m4l321.png', // .NET Framework 图标
     name: '.NET Framework',
     title: '.NET框架',
     desc: 'Microsoft的.NET应用程序运行和开发框架',
+    orgLink: 'https://dotnet.microsoft.com/download/dotnet-framework',
+    org: 'microsoft',
   },
 ];
 
@@ -299,12 +377,16 @@ export const regexUtility = [
     name: 'RegexBuddy 4',
     title: '正则表达式工具',
     desc: '可视化的正则表达式编写和测试工具',
+    orgLink: 'http://www.regexbuddy.com',
+    org: 'regexbuddy',
   },
   {
     avatar: 'https://i.imgur.com/8o6n543.png', // Match Tracer 图标
     name: 'Match Tracer',
     title: '正则表达式工具',
     desc: '正则表达式调试和分析工具',
+    orgLink: 'http://www.regexlab.com/zh/mtracer/',
+    org: 'regexlab',
   },
 ];
 
@@ -315,18 +397,24 @@ export const webApiUtility = [
     name: 'ApiPost',
     title: 'API调试工具',
     desc: '集API调试、管理、文档生成于一体的工具',
+    orgLink: 'https://www.apipost.cn/download.html',
+    org: 'apipost',
   },
   {
     avatar: 'https://i.imgur.com/0q8p765.png', // Postwoman 图标
     name: 'Postwoman',
     title: 'API调试工具',
     desc: '开源免费的API调试和管理工具',
+    orgLink: 'https://postwoman.io/',
+    org: 'postwoman',
   },
   {
     avatar: 'https://i.imgur.com/1r9q876.png', // Postman 图标
     name: 'Postman',
     title: 'API调试工具',
     desc: '功能强大的API开发和测试协作平台',
+    orgLink: 'https://www.postman.com/',
+    org: 'postman',
   },
 ];
 
@@ -337,18 +425,24 @@ export const snipasteUtility = [
     name: 'Snipaste',
     title: '截图工具',
     desc: '轻量级截图工具，支持贴图功能',
+    orgLink: 'https://www.snipaste.com/',
+    org: 'snipaste',
   },
   {
     avatar: 'https://i.imgur.com/3t1s098.png', // FastStone Capture 图标
     name: 'FastStone Capture',
     title: '截图录屏工具',
     desc: '集截图、录屏、编辑于一体的工具',
+    orgLink: 'https://faststone-capture.en.softonic.com/',
+    org: 'softonic',
   },
   {
     avatar: 'https://i.imgur.com/4u2t109.png', // ShareX 图标
     name: 'ShareX',
     title: '截图录屏OCR工具',
     desc: '开源免费的截图、录屏和OCR识别工具',
+    orgLink: 'https://sharex.en.softonic.com/',
+    org: 'softonic',
   },
 ];
 
@@ -359,6 +453,8 @@ export const vscodeAutoComplete = [
     name: 'Kite',
     title: 'VSCode自动补全工具',
     desc: '为VSCode提供智能代码自动补全的工具',
+    orgLink: 'https://www.kite.com/download/',
+    org: 'kite',
   },
 ];
 
@@ -369,6 +465,8 @@ export const webCdnUtility = [
     name: 'BootCDN',
     title: '前端CDN加速服务',
     desc: '稳定、快速、免费的前端开源项目CDN加速服务',
+    orgLink: 'https://www.bootcdn.cn/',
+    org: 'bootcdn',
   },
 ];
 
@@ -379,11 +477,15 @@ export const wpfPackageUtility = [
     name: 'Inno Setup',
     title: 'Windows安装包制作工具',
     desc: '免费的Windows安装程序制作工具',
+    orgLink: 'https://jrsoftware.org/isdl.php/',
+    org: 'jrsoftware',
   },
   {
     avatar: 'https://i.imgur.com/8y6x543.png', // NSIS 图标
     name: 'NSIS',
     title: 'Windows安装包制作工具',
     desc: 'Nullsoft Scriptable Install System，开源的安装包制作工具',
+    orgLink: 'https://nsis.sourceforge.io/Download',
+    org: 'sourceforge',
   },
 ];
